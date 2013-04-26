0c20ead29203215af7eef0fb1abca88c98366e0a
Casen <cdubd10@gmail.com>
2013-04-25 23:04:38 -0400

    enhancing parsing

--- csv_to_json.js
+++ csv_to_json.js
@@ -22,11 +22,24 @@ csv()
   row.unshift(row.pop());
   row.shift();
 
+  //trim excess whitespace
+  row = _.map(row, function(val){
+    return val.trim();
+  });
+
   //if a row has content, let's transform it!
   if (row.length > 0){
     if(index == 0){
       headerRow = row;
     } else {
+      // Convert numbers to float values, leave strings alone
+      row = _.map(row, function(val){
+        if(isNaN(parseFloat(val)) || val.split(' ').length > 1)
+          return val;
+        else {
+          return parseFloat(val);
+        }
+      });
       //convert rows into json objects with headers as keys
       obj = _.object(headerRow, row);
       return JSON.stringify(obj) + ',';
